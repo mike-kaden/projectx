64009cc657d1a5461dd51e6fc6f75b772df0647f
Mike Kaden <mike.kaden@hereiam.solutions>
2022-10-04 10:46:21 +0200

    clean up

--- projetx-web/src/components/Home/Home.jsx
+++ projetx-web/src/components/Home/Home.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import styled from "styled-components";
 
-import { useTranslation } from "react-i18next";
-
 import Header from "./Header/Header";
 import Nav from "../nav";
 
